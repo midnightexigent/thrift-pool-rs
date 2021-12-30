9df213fbc23f4a33b6c151613df3518eec457866
Midnight Exigent <36641328+midnightexigent@users.noreply.github.com>
2021-12-30 05:49:31 +0100

    Update

--- README.md
+++ README.md
@@ -1,18 +1,136 @@
-# Thrift Pool 
+# Thrift Pool
 
-This library provides the `ThriftConnectionManager` type which makes it easy to implement Connection Pools for any [thrift client](https://docs.rs/thrift/latest/thrift/)
+This library provides a way easily to implement Connection Pools for any [`thrift::TThriftClient`],
+which can then be used alongside [`bb8`] and/or [`r2d2`]
 
-This library was extracted from [hbase-thrift](https://crates.io/crates/hbase-thrift)
+<br>
 
 ## Usage
 
-- Implement the `FromIoProtocol`, `IsValid` and the `HasBroken` traits for your `thrift` client
-- That's it ! Now you are able to create a `ThriftConnectionManager` with your `thrift`
-- `ThriftConnectionManager` can create new instances of the client through [bb8](https://github.com/djc/bb8/) or [r2d2](https://github.com/sfackler/r2d2)
+There are 2 possible use cases
 
-## Examples
+### As a library
 
-- See [hbase-thrift-rs](https://github.com/midnightexigent/hbase-thrift-rs)
-- See [thrift-pool-tutorial](https://github.com/midnightexigent/thrift-pool-tutorial-rs) which showcases how to implement a connection pool around the client used in the [official tutorial](https://github.com/apache/thrift/tree/master/tutorial) (`CalculatorSyncClient`) using the capabilities of this library
+If you're implementing a library that provides a (possibly generated) thrift client,
+you should implement the [`ThriftConnection`] and [`FromProtocol`] traits
+for that client
 
+```rust
+# use thrift::protocol::{TInputProtocol, TOutputProtocol};
+# use thrift_pool::{FromProtocol, ThriftConnection};
+#
+// A typical generated client looks like this
+struct MyThriftClient<Ip: TInputProtocol, Op: TOutputProtocol> {
+    i_prot: Ip,
+    o_prot: Op,
+}
+
+impl<Ip: TInputProtocol, Op: TOutputProtocol> FromProtocol for MyThriftClient<Ip, Op> {
+    type InputProtocol = Ip;
+
+    type OutputProtocol = Op;
+
+    fn from_protocol(
+        input_protocol: Self::InputProtocol,
+        output_protocol: Self::OutputProtocol,
+    ) -> Self {
+        MyThriftClient {
+            i_prot: input_protocol,
+            o_prot: output_protocol,
+        }
+    }
+}
+
+impl<Ip: TInputProtocol, Op: TOutputProtocol> ThriftConnection for MyThriftClient<Ip, Op> {
+    type Error = thrift::Error;
+    fn is_valid(&mut self) -> Result<(), Self::Error> {
+       Ok(())
+    }
+    fn has_broken(&mut self) -> bool {
+       false
+   }
+}
+
+```
+
+### As an application
+
+If you're implementing an application that uses a (possibly generated) thrift client that
+implements [`FromProtocol`] and [`ThriftConnection`] (see previous section), you can use
+[`r2d2`] or [`bb8`] (make sure to read their documentations) along with
+[`ThriftConnectionManager`] to create Connection Pools for the client
+
+```rust should_panic
+# use thrift::protocol::{TInputProtocol, TOutputProtocol};
+# use thrift_pool::{FromProtocol, ThriftConnection};
+#
+# struct MyThriftClient<Ip: TInputProtocol, Op: TOutputProtocol> {
+#     i_prot: Ip,
+#     o_prot: Op,
+# }
+#
+# impl<Ip: TInputProtocol, Op: TOutputProtocol> FromProtocol for MyThriftClient<Ip, Op> {
+#     type InputProtocol = Ip;
+#
+#     type OutputProtocol = Op;
+#
+#     fn from_protocol(
+#         input_protocol: Self::InputProtocol,
+#         output_protocol: Self::OutputProtocol,
+#     ) -> Self {
+#         MyThriftClient {
+#             i_prot: input_protocol,
+#             o_prot: output_protocol,
+#         }
+#     }
+# }
+#
+# impl<Ip: TInputProtocol, Op: TOutputProtocol> ThriftConnection for MyThriftClient<Ip, Op> {
+#     type Error = thrift::Error;
+#     fn is_valid(&mut self) -> Result<(), Self::Error> {
+#        Ok(())
+#     }
+#     fn has_broken(&mut self) -> bool {
+#        false
+#    }
+# }
+# use thrift_pool::{MakeThriftConnectionFromAddrs};
+# use thrift::protocol::{TCompactInputProtocol, TCompactOutputProtocol};
+# use thrift::transport::{
+#    ReadHalf, TFramedReadTransport, TFramedWriteTransport, TTcpChannel, WriteHalf,
+# };
+#
+type Client = MyThriftClient<
+    TCompactInputProtocol<TFramedReadTransport<ReadHalf<TTcpChannel>>>,
+    TCompactOutputProtocol<TFramedWriteTransport<WriteHalf<TTcpChannel>>>,
+>;
+
+# #[tokio::main]
+# async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  // create a connection manager
+  let manager = MakeThriftConnectionFromAddrs::<Client, _>::new("localhost:9090").into_connection_manager();
+  
+  // we're able to create bb8 and r2d2 Connection Pools
+  let bb8 = bb8::Pool::builder().build(manager.clone()).await?;
+  let r2d2 = r2d2::Pool::builder().build(manager)?;
+
+  // get a connection
+  let conn1 = bb8.get().await?;
+  let conn2 = r2d2.get()?;
+#  Ok(())
+# }
+```
+
+<br>
+
+## More examples
+
+- [hbase-thrift](https://github.com/midnightexigent/hbase-thrift-rs) -- the project from which this
+library was extracted. implements Connection Pools for the client generated from the
+[HBase Thrift Spec](https://github.com/apache/hbase/tree/master/hbase-thrift/src/main/resources/org/apache/hadoop/hbase/thrift)
+- [thrift-pool-tutorial](https://github.com/midnightexigent/thrift-pool-tutorial-rs) -- implements
+Connection Pools for the client used in the official
+[thrift tutorial](https://github.com/apache/thrift/tree/master/tutorial)
+
+<br>
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../README.md")]
+
 use std::{
     io::{Read, Write},
     marker::PhantomData,
@@ -13,78 +15,92 @@ use thrift::{
         ReadHalf, TBufferedReadTransport, TBufferedWriteTransport, TFramedReadTransport,
         TFramedWriteTransport, TIoChannel, TReadTransport, TTcpChannel, TWriteTransport, WriteHalf,
     },
-    TThriftClient,
 };
 
-pub trait ReadTransportExt: TReadTransport {
+/// Create self from a [`Read`]
+pub trait FromRead: TReadTransport {
     type Read: Read;
     fn from_read(read: Self::Read) -> Self;
 }
-impl<R: Read> ReadTransportExt for TBufferedReadTransport<R> {
+
+impl<R: Read> FromRead for TBufferedReadTransport<R> {
     type Read = R;
     fn from_read(read: R) -> Self {
         Self::new(read)
     }
 }
-impl<R: Read> ReadTransportExt for TFramedReadTransport<R> {
+impl<R: Read> FromRead for TFramedReadTransport<R> {
     type Read = R;
     fn from_read(read: R) -> Self {
         Self::new(read)
     }
 }
-pub trait WriteTransportExt: TWriteTransport {
+
+/// Create self from a [`Write`]
+pub trait FromWrite: TWriteTransport {
     type Write: Write;
     fn from_write(write: Self::Write) -> Self;
 }
-impl<W: Write> WriteTransportExt for TBufferedWriteTransport<W> {
+
+impl<W: Write> FromWrite for TBufferedWriteTransport<W> {
     type Write = W;
     fn from_write(write: W) -> Self {
         Self::new(write)
     }
 }
-impl<W: Write> WriteTransportExt for TFramedWriteTransport<W> {
+
+impl<W: Write> FromWrite for TFramedWriteTransport<W> {
     type Write = W;
 
     fn from_write(write: Self::Write) -> Self {
         Self::new(write)
     }
 }
-pub trait InputProtocolExt: TInputProtocol {
+
+/// Create self from a [`TReadTransport`]
+pub trait FromReadTransport: TInputProtocol {
     type ReadTransport: TReadTransport;
     fn from_read_transport(r_tran: Self::ReadTransport) -> Self;
 }
-impl<Rt: TReadTransport> InputProtocolExt for TBinaryInputProtocol<Rt> {
+
+impl<Rt: TReadTransport> FromReadTransport for TBinaryInputProtocol<Rt> {
     type ReadTransport = Rt;
 
     fn from_read_transport(r_tran: Rt) -> Self {
         Self::new(r_tran, true)
     }
 }
-impl<Rt: TReadTransport> InputProtocolExt for TCompactInputProtocol<Rt> {
+
+impl<Rt: TReadTransport> FromReadTransport for TCompactInputProtocol<Rt> {
     type ReadTransport = Rt;
 
     fn from_read_transport(r_tran: Rt) -> Self {
         Self::new(r_tran)
     }
 }
-pub trait OutputProtocolExt: TOutputProtocol {
+
+/// Create self from a [`TWriteTransport`]
+pub trait FromWriteTransport: TOutputProtocol {
     type WriteTransport: TWriteTransport;
     fn from_write_transport(w_tran: Self::WriteTransport) -> Self;
 }
-impl<Wt: TWriteTransport> OutputProtocolExt for TBinaryOutputProtocol<Wt> {
+
+impl<Wt: TWriteTransport> FromWriteTransport for TBinaryOutputProtocol<Wt> {
     type WriteTransport = Wt;
     fn from_write_transport(w_tran: Wt) -> Self {
         Self::new(w_tran, true)
     }
 }
-impl<Wt: TWriteTransport> OutputProtocolExt for TCompactOutputProtocol<Wt> {
+
+impl<Wt: TWriteTransport> FromWriteTransport for TCompactOutputProtocol<Wt> {
     type WriteTransport = Wt;
     fn from_write_transport(w_tran: Wt) -> Self {
         Self::new(w_tran)
     }
 }
 
-pub trait ThriftClientExt: TThriftClient {
+/// Create self from a [`TInputProtocol`] and a [`TOutputProtocol`]
+pub trait FromProtocol {
     type InputProtocol: TInputProtocol;
     type OutputProtocol: TOutputProtocol;
 
@@ -94,39 +110,135 @@ pub trait ThriftClientExt: TThriftClient {
     ) -> Self;
 }
 
+/// Checks the validity of the connection
+///
+/// Used by [`ThriftConnectionManager`] to implement parts of
+/// [`bb8::ManageConnection`] and/or [`r2d2::ManageConnection`]
 pub trait ThriftConnection {
+    /// See [r2d2::ManageConnection::Error] and/or [bb8::ManageConnection::Error]
     type Error;
+    /// See [r2d2::ManageConnection::is_valid] and/or [bb8::ManageConnection::is_valid]
     fn is_valid(&mut self) -> Result<(), Self::Error>;
-    fn has_broken(&mut self) -> bool;
+    /// See [r2d2::ManageConnection::has_broken] and/or [bb8::ManageConnection::has_broken]
+    fn has_broken(&mut self) -> bool {
+        false
+    }
 }
 
+/// A trait that creates new [`ThriftConnection`]s
+///
+/// Used by [`ThriftConnectionManager`] to implement
+/// [`bb8::ManageConnection::connect`]
+/// and/or [`r2d2::ManageConnection::connect`]
 pub trait MakeThriftConnection {
+    /// The error type returned when a connection creation fails
     type Error;
+    /// The connection type the we are trying to create
     type Output;
+    /// Attempt to create a new connection
     fn make_thrift_connection(&self) -> Result<Self::Output, Self::Error>;
 }
 
+/// A [`MakeThriftConnection`] that attempts to create new connections
+/// from a [`ToSocketAddrs`] and a [`FromProtocol`]
+///
+/// The connection is accordance with the
+/// [thrift rust tutorial](https://github.com/apache/thrift/tree/master/tutorial):
+///
+/// * Open a [`TTcpChannel`]
+/// * Split it
+/// * Create `TReadTransport` and `TWriteTransport`
+/// * Create `TInputProtocol` and `TOutputProtocol`
+/// * Create a client with `i_prot` and `o_prot`
+///
+/// For that to happen, `T` needs to be able
+/// to create the `Read`/`Write` `Transport`s
+/// and `Input`/`Output` `Protocol`s from
+/// the `ReadHalf` and `WriteHalf` of the `TTcpChannel`.
+/// Those contraints should be fairly easily satisfied
+/// by implementing the relevant traits in the library
+///
+/// ```
+///
+/// use thrift_pool::{MakeThriftConnectionFromAddrs, FromProtocol};
+///
+/// use thrift::{
+///     protocol::{TCompactInputProtocol, TCompactOutputProtocol, TInputProtocol, TOutputProtocol},
+///     transport::{
+///         ReadHalf, TFramedReadTransport, TFramedWriteTransport, TIoChannel, TReadTransport,
+///         TTcpChannel, TWriteTransport, WriteHalf,
+///     },
+/// };
+///
+/// // A typical generated client looks like this
+/// struct MyThriftClient<Ip: TInputProtocol, Op: TOutputProtocol> {
+///     i_prot: Ip,
+///     o_prot: Op,
+/// }
+/// impl<Ip: TInputProtocol, Op: TOutputProtocol> FromProtocol for MyThriftClient<Ip, Op> {
+///     type InputProtocol = Ip;
+///
+///     type OutputProtocol = Op;
+///
+///     fn from_protocol(
+///         input_protocol: Self::InputProtocol,
+///         output_protocol: Self::OutputProtocol,
+///     ) -> Self {
+///         MyThriftClient {
+///             i_prot: input_protocol,
+///             o_prot: output_protocol,
+///         }
+///     }
+/// }
+/// type Client = MyThriftClient<
+///     TCompactInputProtocol<TFramedReadTransport<ReadHalf<TTcpChannel>>>,
+///     TCompactOutputProtocol<TFramedWriteTransport<WriteHalf<TTcpChannel>>>,
+/// >;
+///
+/// // The Protocols/Transports used in this client implement the necessary traits so we can do this
+/// let manager =
+///     MakeThriftConnectionFromAddrs::<Client, _>::new("localhost:9090").into_connection_manager();
+///
+/// ```
+
 pub struct MakeThriftConnectionFromAddrs<T, S> {
     addrs: S,
-    _t: PhantomData<T>,
+    conn: PhantomData<T>,
+}
+
+impl<T, S: std::fmt::Debug> std::fmt::Debug for MakeThriftConnectionFromAddrs<T, S> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("MakeThriftConnectionFromAddrs")
+            .field("addrs", &self.addrs)
+            .field("conn", &self.conn)
+            .finish()
+    }
+}
+impl<T, S: Clone> Clone for MakeThriftConnectionFromAddrs<T, S> {
+    fn clone(&self) -> Self {
+        Self {
+            addrs: self.addrs.clone(),
+            conn: PhantomData,
+        }
+    }
 }
 
 impl<T, S> MakeThriftConnectionFromAddrs<T, S> {
     pub fn new(addrs: S) -> Self {
         Self {
             addrs,
-            _t: PhantomData,
+            conn: PhantomData,
         }
     }
 }
 
 impl<
         S: ToSocketAddrs + Clone,
-        Rt: ReadTransportExt<Read = ReadHalf<TTcpChannel>>,
-        Ip: InputProtocolExt<ReadTransport = Rt>,
-        Wt: WriteTransportExt<Write = WriteHalf<TTcpChannel>>,
-        Op: OutputProtocolExt<WriteTransport = Wt>,
-        T: ThriftClientExt<InputProtocol = Ip, OutputProtocol = Op>,
+        Rt: FromRead<Read = ReadHalf<TTcpChannel>>,
+        Ip: FromReadTransport<ReadTransport = Rt>,
+        Wt: FromWrite<Write = WriteHalf<TTcpChannel>>,
+        Op: FromWriteTransport<WriteTransport = Wt>,
+        T: FromProtocol<InputProtocol = Ip, OutputProtocol = Op>,
     > MakeThriftConnectionFromAddrs<T, S>
 {
     pub fn into_connection_manager(self) -> ThriftConnectionManager<Self> {
@@ -136,11 +248,11 @@ impl<
 
 impl<
         S: ToSocketAddrs + Clone,
-        Rt: ReadTransportExt<Read = ReadHalf<TTcpChannel>>,
-        Ip: InputProtocolExt<ReadTransport = Rt>,
-        Wt: WriteTransportExt<Write = WriteHalf<TTcpChannel>>,
-        Op: OutputProtocolExt<WriteTransport = Wt>,
-        T: ThriftClientExt<InputProtocol = Ip, OutputProtocol = Op>,
+        Rt: FromRead<Read = ReadHalf<TTcpChannel>>,
+        Ip: FromReadTransport<ReadTransport = Rt>,
+        Wt: FromWrite<Write = WriteHalf<TTcpChannel>>,
+        Op: FromWriteTransport<WriteTransport = Wt>,
+        T: FromProtocol<InputProtocol = Ip, OutputProtocol = Op>,
     > MakeThriftConnection for MakeThriftConnectionFromAddrs<T, S>
 {
     type Error = thrift::Error;
@@ -162,7 +274,23 @@ impl<
     }
 }
 
+/// An implementor of [`bb8::ManageConnection`] and/or [`r2d2::ManageConnection`].
+/// `T` should a [`MakeThriftConnection`] and `T::Output` should be a [`ThriftConnection`]
 pub struct ThriftConnectionManager<T>(T);
+
+impl<T: Clone> Clone for ThriftConnectionManager<T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+impl<T: std::fmt::Debug> std::fmt::Debug for ThriftConnectionManager<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("ThriftConnectionManager")
+            .field(&self.0)
+            .finish()
+    }
+}
+
 impl<T> ThriftConnectionManager<T> {
     pub fn new(make_thrift_connection: T) -> Self {
         Self(make_thrift_connection)
